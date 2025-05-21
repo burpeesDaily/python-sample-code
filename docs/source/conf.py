7183d68e4a83ea618a4e273ad1e4c5923420730d
Shun <shun@formosa1544.com>
2025-05-20 22:40:02 -0700

    Fix linting errors.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,25 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sys
-from pathlib import Path
 
-project = 'Sphinx Sample Code'
-copyright = '2025, Shun Huang'
-author = 'Shun Huang'
+project = "Sphinx Sample Code"
+copyright = "2025, Shun Huang"
+author = "Shun Huang"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx_copybutton", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
